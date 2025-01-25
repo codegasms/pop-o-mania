a096523b9bf4df81a04c1a68b1380abbf48a59ba
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-01-25 14:58:55 +0530

    [Add] Implement filesystem utility functions for directory listing and management

---
+++ src/fs.cpp
@@ -0,0 +1,44 @@
+#include "fs.hpp"
+
+#include "globals.hpp"
+
+namespace fs = std::filesystem;
+
+std::vector<std::string> ls(char* extension) {
+    for (int i = 0; i < Global.Path.size(); i++) {
+        if (Global.Path[i] == '\\') {
+            Global.Path[i] = '/';
+        }
+    }
+    std::vector<std::string> text;
+    text.clear();
+    for (const auto& entry : fs::directory_iterator(Global.Path)) {
+        std::string filename = entry.path().filename().string();
+        fs::directory_entry isDirectory(entry.path());
+        if (filename[0] != '.') {
+            if (isDirectory.is_directory()) {
+                filename.push_back('/');
+                text.push_back(filename);
+            } else if (entry.path().extension() == extension) {
+                text.push_back(filename);
+            }
+        }
+    }
+    sort(text.begin(), text.end());
+    return text;
+}
+
+void create_dir(const std::string& path) { fs::create_directory(path); }
+
+std::string get_without_ext(const std::string& path) {
+    fs::path path_p(Global.selectedPath);
+    return path_p.stem().string();
+}
+
+int on_extract_entry(const char* filename, void* arg) {
+    int i = 0;
+    int n = *(int*)arg;
+    printf("Extracted: %s (%d of %d)\n", filename, ++i, n);
+
+    return 0;
+}
