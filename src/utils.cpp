11e33108c44e8d01b61a7c39d878a1afdf2b3a7a
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-01-25 14:42:28 +0530

    [Add] Implement drawing functions with scaling and rotation capabilities

--- src/utils.cpp
+++ src/utils.cpp
@@ -124,26 +124,52 @@ Rectangle GetRaylibOriginR(Rectangle a) {
 // ============== Drawing functions ============== //
 
 // Draw a centered and scaled Texture
-void DrawTextureCenter(Texture2D, float, float, float, Color);
+void DrawTextureCenter(Texture2D tex, float x, float y, float s, Color color) {
+    DrawTextureEx(tex, ScaleCords(GetRaylibOrigin({x, y, tex.width * s, tex.height * s})), 0,
+                  Scale(s), color);
+}
 
 // Draw centered and scaled combo numbers
 void DrawCNumbersCenter(int, float, float, float, Color);
 
-// Draw a centered/left and scaled Texture with a rotation
-void DrawTextCenter(const char *, float, float, float, Color);
-void DrawTextLeft(const char *, float, float, float, Color);
-
 // Draw scaled combo numbers from left to right
 void DrawCNumbersLeft(int, float, float, float, Color);
 
+// Draw a centered/left and scaled Texture with a rotation
+void DrawTextCenter(const char *text, float x, float y, float s, Color color) {
+    Vector2 size = MeasureTextEx(Global.DefaultFont, text, s, 1);
+    DrawTextPro(Global.DefaultFont, text, ScaleCords(Vector2{x - size.x / 2.0f, y - size.y / 2.0f}),
+                Vector2{0, 0}, 0, Scale(s), Scale(1), color);
+}
+void DrawTextLeft(const char *text, float x, float y, float s, Color color) {
+    // draw text LTR
+    Vector2 size = MeasureTextEx(Global.DefaultFont, text, s, 1);
+    DrawTextPro(Global.DefaultFont, text, ScaleCords(Vector2{x, y - size.y / 2.0f}), Vector2{0, 0},
+                0, Scale(s), Scale(1), color);
+}
+
 // Draw a centered, scaled and rotated Texture
-void DrawTextureRotate(Texture2D, float, float, float, float, Color);
+void DrawTextureRotate(Texture2D tex, float x, float y, float s, float r, Color color) {
+    DrawTexturePro(
+        tex, Rectangle{0, 0, tex.width, tex.height},
+        Rectangle{ScaleCordX(x), ScaleCordY(y), Scale(tex.width * s), Scale(tex.height * s)},
+        Vector2{Scale(tex.width * s / 2.0f), Scale(tex.height * s / 2.0f)}, r, color);
+}
 
 // Draw a centered, scaled and rotated Texture with a rectangle
-void DrawTextureOnCircle(Texture2D, float, float, float, float, float, float, Color);
+void DrawTextureOnCircle(Texture2D tex, float x, float y, float rad, float s, float r, float ang,
+                         Color color) {
+    Vector2 pos = getPointOnCircle(x, y, rad, ang);
+    DrawTextureRotate(tex, pos.x, pos.y, s, r, color);
+}
 
 // Draw a slider texture (exclusively for a slider object)
-void DrawTextureSlider(Texture2D, float, float, Color, float);
+void DrawTextureSlider(Texture2D tex, float x, float y, Color color, float s) {
+    DrawTextureEx(tex,
+                  ScaleCords({x - s / 2.0f - 4.0f * Global.sliderTexSize,
+                              y - s / 2.0f - 4.0f * Global.sliderTexSize}),
+                  0, Scale(1.0f / Global.sliderTexSize), color);
+}
 
 void DrawSpinnerMeter(Texture2D, float);
 
@@ -152,23 +178,52 @@ void DrawSpinnerBack(Texture2D, Color);
 // ============== Math functions ============== //
 
 // Clip a number between two other numbers
-float clip(float, float, float);
+float clip(float n, float lower, float upper) {
+    n = (n > lower) * n + !(n > lower) * lower;
+    return (n < upper) * n + !(n < upper) * upper;
+}
 
 // Interpolate more points in between two points
-Vector2 lerp(Vector2, Vector2, float);
+Vector2 lerp(Vector2 a, Vector2 b, float t) {
+    return {.x = (1 - t) * a.x + t * b.x, .y = (1 - t) * a.y + t * b.y};
+}
 
 // Return a vector with both of the cordinates set to a float
-Vector2 vectorize(float);
+Vector2 vectorize(float i) { return Vector2{i, i}; }
 
 // Calculate the distance between two points
-float distance(Vector2 &, Vector2 &);
+float distance(Vector2 &p0, Vector2 &p1) {
+    return std::sqrt(std::pow(std::abs(p0.x - p1.x), 2) + std::pow(std::abs(p0.y - p1.y), 2));
+}
 
 // Binary search algorithm
-int Search(std::vector<float>, float, int, int);
+int Search(std::vector<float> arr, float x, int l, int r) {
+    if (r >= l) {
+        int mid = (l + r) / 2;
+        if (arr[mid] == x || l == r) return mid;
+        if (arr[mid] > x)
+            return Search(arr, x, l, mid - 1);
+        else
+            return Search(arr, x, mid + 1, r);
+    } else
+        return l;
+}
 
-Vector2 getPointOnCircle(float, float, float, float);
+Vector2 getPointOnCircle(float x, float y, float radius, float angle) {
+    angle = (angle * M_PI) / 180;
+    float xdiff = radius * cos(angle);
+    float ydiff = radius * sin(angle);
+    return Vector2{x + xdiff, y + ydiff};
+}
 
-std::string getSampleSetFromInt(int);
+std::string getSampleSetFromInt(int s) {
+    if (s == 1)
+        return "normal";
+    else if (s == 2)
+        return "soft";
+    else if (s == 3)
+        return "drum";
+}
 
 // ============== Extra Utility functions ============== //
 
